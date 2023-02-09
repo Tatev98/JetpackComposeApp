ca2be37e9a55680c23f996e6cbb95ea5471b4e9d
tatevikvardanyan <tatevik.vardanyan.98@gmail.com>
2023-02-10 03:13:08 +0400

    Create separate alert and snackbar

---
+++ app/src/main/java/com/example/jetpackcomposeapp/screens/dialogs/DefaultAlert.kt
@@ -0,0 +1,50 @@
+package com.example.jetpackcomposeapp.screens.dialogs
+
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.example.jetpackcomposeapp.ui.theme.primaryBlue
+
+
+//Show usual alert dialog with 2 buttons
+@Composable
+fun DefaultAlert(
+    title: String,
+    text: String,
+    confirmBtnText: String,
+    cancelBtnText: String,
+    onDismiss: (Boolean) -> Unit //set onDismiss value true if clicked confirm btn else set false
+) {
+    AlertDialog(
+        onDismissRequest = {
+            onDismiss(false)
+        },
+        title = {
+            Text(text = title, color = Color.Black)
+        },
+        text = {
+            Text(
+                text,
+                color = Color.Black
+            )
+        },
+        confirmButton = {
+            Button(
+                colors = ButtonDefaults.buttonColors(backgroundColor = primaryBlue),
+                onClick = {
+                    onDismiss(true)
+                },
+            ) {
+                Text(confirmBtnText, color = Color.White)
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismiss(false)
+                }) {
+                Text(cancelBtnText, color = primaryBlue)
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/example/jetpackcomposeapp/screens/dialogs/DefaultSnackbar.kt
@@ -0,0 +1,48 @@
+package com.example.jetpackcomposeapp.screens.dialogs
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+
+//show usual snackbar with icon if need
+@Composable
+fun DefaultSnackbar(
+    snackbarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+    showIcon: Boolean,
+    icon: Int
+) {
+    SnackbarHost(
+        modifier = modifier,
+        hostState = snackbarHostState,
+        snackbar = { data ->
+            Snackbar(
+                backgroundColor = Color.DarkGray,
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.DarkGray),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(data.message, color = Color.White)
+                    if (showIcon) {
+                        Icon(
+                            painterResource(id = icon),
+                            contentDescription = null,
+                            tint = Color.Green
+                        )
+                    }
+                }
+            }
+        })
+}
+
